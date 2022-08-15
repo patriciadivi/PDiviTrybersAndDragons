a84a9141fce1e0ea9f8b485180bac4dc337be385
PatriciaDivi <patriciavianad@gmail.com>
2022-08-15 15:23:08 -0300

    feat: implementado class 'Mage' no arquivo 'Mage.ts', na pasta 'Archetypes' :rocket:

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,16 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private _energyType: EnergyType = 'mana';
+  private static _Icount = 0;
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._Icount += 1;
+    return this._Icount;
+  }
+}
