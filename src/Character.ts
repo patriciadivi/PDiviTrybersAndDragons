8a7a3314b46422eb8bbd1733d698de736b0593d0
PatriciaDivi <patriciavianad@gmail.com>
2022-08-15 22:14:20 -0300

    Implentado a class 'Character' no arquivo 'Character.ts' na pasta 'src' :rocket:

--- src/Character.ts
+++ src/Character.ts
@@ -2,7 +2,7 @@ import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
 import Race, { Elf } from './Races';
-import randomNumberFrom1To10 from './Utils/randomNumberFrom1To10';
+import getRandomInt from './utils';
 
 export default class Character implements Fighter {
   private _race: Race;
@@ -17,17 +17,17 @@ export default class Character implements Fighter {
 
   constructor(name: string) {
     this._archetype = new Mage(name);
-    this._strength = randomNumberFrom1To10();
-    this._defense = randomNumberFrom1To10();
-    this._energy = {
-      type_: this._archetype.energyType,
-      amount: randomNumberFrom1To10(),
-    };
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
     this._name = name;
-    this._dexterity = randomNumberFrom1To10();
+    this._dexterity = getRandomInt(1, 10);
     this._race = new Elf(this._name, this._dexterity);
     this._maxLifePoints = this._race.maxLifePoints / 2;
-    this._lifePoints = this._race.maxLifePoints;
+    this._lifePoints = this._maxLifePoints;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
   }
 
   get archetype(): Archetype { 
@@ -77,16 +77,26 @@ export default class Character implements Fighter {
   }
 
   levelUp(): void {
-    this._maxLifePoints += randomNumberFrom1To10();
-    
+    // const lifePlus = getRandomInt(1, 10);
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._energy.amount = 10;
+
+    // if (this._maxLifePoints + lifePlus > this._race.maxLifePoints) {
+    //   this._maxLifePoints = this._race.maxLifePoints;
+    //   this._lifePoints = this._maxLifePoints;
+    // } else {
+    //   this._maxLifePoints += lifePlus;
+    //   this._lifePoints = this._maxLifePoints;
+    // }
+
     if (this._maxLifePoints > this._race.maxLifePoints) {
       this._maxLifePoints = this._race.maxLifePoints;
+      this._lifePoints = this._maxLifePoints;
     }
     this._lifePoints = this._maxLifePoints;
-    this._energy.amount = 10;
-    this._strength += randomNumberFrom1To10();
-    this._defense += randomNumberFrom1To10();
-    this._dexterity += randomNumberFrom1To10();
   }
 
   receiveDamage(attackPoints: number): number {
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// import Character from './Character';
+
+// const newCharacter = new Character('tata');
+// console.log(newCharacter.maxLifePoints, 'maxLifePoints');
+// console.log(newCharacter.lifePoints);
+// newCharacter.levelUp();
+// console.log(newCharacter.maxLifePoints);
+// console.log(newCharacter.lifePoints);
